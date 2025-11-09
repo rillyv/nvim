e12dea8a7d3c2c795e3a511dd98491cac33bf033
Anthony Mamaril <rillyv.dev@gmail.com>
2025-11-09 22:40:27 +0100

    Update

--- lua/rillyv/lazy/telescope.lua
+++ lua/rillyv/lazy/telescope.lua
@@ -14,7 +14,7 @@ return {
 		local utils = require("telescope.utils")
 
 		vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
-		vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+		vim.keymap.set("n", "<C-g>", builtin.git_files, {})
 		vim.keymap.set("n", "<leader>pws", function()
 			local word = vim.fn.expand("<cword>")
 			builtin.grep_string({ search = word })
--- lua/rillyv/remap.lua
+++ lua/rillyv/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", "<cmd>Oil<CR>")
 
 vim.keymap.set("n", "x", '"_x')
 
